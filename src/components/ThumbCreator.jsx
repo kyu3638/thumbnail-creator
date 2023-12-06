b59179020efe5d1413502b0b6e85b022f8d1d1b4
kyu3638 <kyu3638@gmail.com>
2023-12-06 13:32:48 +0900

    feat:썸네일 생성, localstorage 저장

---
+++ src/components/ThumbCreator.jsx
@@ -0,0 +1,225 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+
+/** localStorage 이용을 위한 유틸리티 함수 */
+import storage from '../utils/storage/storage';
+
+function ThumbCreator() {
+  // thumbnail state
+  const [width, setWidth] = useState(300);
+  const [height, setHeight] = useState(200);
+  const [background, setBackground] = useState(
+    `url(https://images.unsplash.com/photo-1685895324391-ba9e104fd2d0?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)`
+  );
+  // text
+  const [title, setTitle] = useState('제목을 입력하세요');
+  const [subtitle, setSubtitle] = useState('소제목을 입력하세요');
+  const [titleSize, setTitleSize] = useState(30);
+  const [subtitleSize, setSubtitleSize] = useState(20);
+  const [isBold, setIsBold] = useState(false);
+  const [isShadow, setIsShadow] = useState(false);
+  const [isBlack, setIsBlack] = useState(false);
+
+  /** 사용자가 선택한 옵션에 따라 배경을 그리는 함수 */
+  function setBackgroundImage(event) {
+    /** 랜덤한 rgb 색상 생성하는 함수  */
+    function makeRGB() {
+      const a = Math.floor(Math.random() * 90 + 1) + 150;
+      const b = Math.floor(Math.random() * 90 + 1) + 150;
+      const c = Math.floor(Math.random() * 90 + 1) + 150;
+      const rgb = `rgb(${a},${b},${c})`;
+      return rgb;
+    }
+    /** 2개의 RGB값으로 그라디언트를 만드는 함수 */
+    function makeGradient() {
+      const color1 = makeRGB();
+      const color2 = makeRGB();
+      return `linear-gradient(${color1}, ${color2})`;
+    }
+    switch (event.target.innerText) {
+      case '단색':
+        setBackground(makeRGB());
+        break;
+      case '그라디언트':
+        setBackground(makeGradient());
+        break;
+      case '이미지링크':
+        const imageLink = prompt('이미지 링크를 입력해주세요');
+        setBackground(`url(${imageLink})`);
+        break;
+    }
+  }
+
+  /** 텍스트 굵기 변경하는 함수  */
+  function textBoldHandler() {
+    if (isBold) setIsBold(false);
+    else setIsBold(true);
+  }
+  /** 텍스트 그림자 변경하는 함수 */
+  function textShadowHandler() {
+    if (isShadow) setIsShadow(false);
+    else setIsShadow(true);
+  }
+  /** 텍스트 색상 반전하는 함수 */
+  function textColorHandler() {
+    if (isBlack) setIsBlack(false);
+    else setIsBlack(true);
+  }
+
+  /** 현재 제작 중인 썸네일을 local storage에 임시저장하는 함수 */
+  function saveTemp() {
+    let thumbnailInfo = {
+      width: width,
+      height: height,
+      background: background,
+      title: title,
+      subtitle: subtitle,
+      titleSize: titleSize,
+      subtitleSize: subtitleSize,
+      isBold: isBold,
+      isShadow: isShadow,
+      isBlack: isBlack,
+    };
+    const storedThumbnails = storage.get('thumbnail');
+    console.log(storedThumbnails);
+    storage.set('thumbnail', [thumbnailInfo, ...storedThumbnails]);
+  }
+
+  return (
+    <div className="thumb-creator">
+      <div className="preview">
+        <Canvas
+          className="thumb"
+          width={width}
+          height={height}
+          background={background}
+        >
+          <ThumbTitle
+            titleSize={titleSize}
+            isBold={isBold}
+            isShadow={isShadow}
+            isBlack={isBlack}
+          >
+            {title}
+          </ThumbTitle>
+          <hr className="line"></hr>
+          <ThumbSubtitle
+            subtitleSize={subtitleSize}
+            isBold={isBold}
+            isShadow={isShadow}
+            isBlack={isBlack}
+          >
+            {subtitle}
+          </ThumbSubtitle>
+        </Canvas>
+      </div>
+      <div className="options">
+        <Option num={2}>
+          <h2>사이즈</h2>
+          <input
+            type="number"
+            value={width}
+            onChange={(e) => setWidth(e.target.value)}
+          />
+          <input
+            type="number"
+            value={height}
+            onChange={(e) => setHeight(e.target.value)}
+          />
+        </Option>
+        <Option num={3}>
+          <h2>배경</h2>
+          <button onClick={setBackgroundImage}>단색</button>
+          <button onClick={setBackgroundImage}>그라디언트</button>
+          <button onClick={setBackgroundImage}>이미지링크</button>
+        </Option>
+        <Option num={2}>
+          <h2>텍스트</h2>
+          <input
+            type="text"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+          <input
+            type="text"
+            value={subtitle}
+            onChange={(e) => setSubtitle(e.target.value)}
+          />
+        </Option>
+        <Option num={4}>
+          <h2>텍스트 크기</h2>
+          <button onClick={() => setTitleSize((pre) => pre + 1)}>
+            제목 크게
+          </button>
+          <button onClick={() => setTitleSize((pre) => pre - 1)}>
+            제목 작게
+          </button>
+          <button onClick={() => setSubtitleSize((pre) => pre + 1)}>
+            소제목 크게
+          </button>
+          <button onClick={() => setSubtitleSize((pre) => pre - 1)}>
+            소제목 작게
+          </button>
+        </Option>
+        <Option num={3}>
+          <h2>텍스트 스타일</h2>
+          <button onClick={textBoldHandler}>텍스트 굵게</button>
+          <button onClick={textShadowHandler}>텍스트 그림자</button>
+          <button onClick={textColorHandler}>텍스트 반전</button>
+        </Option>
+      </div>
+      <SaveOptions>
+        <button onClick={saveTemp}>임시저장</button>
+        <button>다운로드</button>
+        <button>클립보드 복사</button>
+      </SaveOptions>
+    </div>
+  );
+}
+
+export default ThumbCreator;
+
+const Canvas = styled.div`
+  width: ${(props) => props.width}px;
+  height: ${(props) => props.height}px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background: ${(props) => props.background};
+  background-position: center;
+  background-size: cover;
+`;
+
+const Option = styled.div`
+  width: 90%;
+  heigth: 15%;
+  display: grid;
+  grid-template-rows: 1;
+  grid-template-columns: 25% repeat(${(props) => +props.num}, 1fr);
+`;
+
+const ThumbTitle = styled.h2`
+  font-size: ${(props) => props.titleSize}px;
+  font-weight: ${(props) => (props.isBold ? 'bold' : 'normal')};
+  text-shadow: ${(props) =>
+    props.isShadow ? '2px 2px 3px rgba(0,0,0,0.5)' : 'unset'};
+  color: ${(props) => (props.isBlack ? 'black' : 'white')};
+`;
+
+const ThumbSubtitle = styled.h3`
+  font-size: ${(props) => props.subtitleSize}px;
+  font-weight: ${(props) => (props.isBold ? 'bold' : 'normal')};
+  text-shadow: ${(props) =>
+    props.isShadow ? '2px 2px 3px rgba(0,0,0,0.5)' : 'unset'};
+  color: ${(props) => (props.isBlack ? 'black' : 'white')};
+`;
+
+const SaveOptions = styled.div`
+  width: 100%;
+  height: 10%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 5%;
+`;
---
+++ src/components/ThumbStorage.jsx
@@ -0,0 +1,77 @@
+import React from 'react';
+import storage from '../utils/storage/storage';
+import styled from 'styled-components';
+
+function ThumbStorage() {
+  const storedThumbnails = storage.get('thumbnail');
+
+  const thumbnailsPreivew = storedThumbnails.map((obj) => {
+    const scale = 360 / obj.width;
+    const scaledWidth = obj.width * scale;
+    const scaledHeight = obj.height * scale;
+    const scaledTitleSize = obj.titleSize * scale;
+    const scaledSubtitleSize = obj.subtitleSize * scale;
+    return (
+      <ScaledCanvas
+        width={scaledWidth}
+        height={scaledHeight}
+        background={obj.background}
+      >
+        <ScaledThumbTitle
+          titleSize={scaledTitleSize}
+          isBold={obj.isBold}
+          isShadow={obj.isShadow}
+          isBlack={obj.isBlack}
+        >
+          {obj.title}
+        </ScaledThumbTitle>
+        <hr className="line"></hr>
+        <ScaledThumbSubtitle
+          subtitleSize={scaledSubtitleSize}
+          isBold={obj.isBold}
+          isShadow={obj.isShadow}
+          isBlack={obj.isBlack}
+        >
+          {obj.subtitle}
+        </ScaledThumbSubtitle>
+      </ScaledCanvas>
+    );
+  });
+
+  return (
+    <div className="thumb-storage">
+      <header>미리보기</header>
+      <div className="thumbs-preview">{thumbnailsPreivew}</div>
+    </div>
+  );
+}
+
+export default ThumbStorage;
+
+const ScaledCanvas = styled.div`
+  width: ${(props) => props.width}px;
+  height: ${(props) => props.height}px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background: ${(props) => props.background};
+  background-position: center;
+  background-size: cover;
+`;
+
+const ScaledThumbTitle = styled.h2`
+  font-size: ${(props) => props.titleSize}px;
+  font-weight: ${(props) => (props.isBold ? 'bold' : 'normal')};
+  text-shadow: ${(props) =>
+    props.isShadow ? '2px 2px 3px rgba(0,0,0,0.5)' : 'unset'};
+  color: ${(props) => (props.isBlack ? 'black' : 'white')};
+`;
+
+const ScaledThumbSubtitle = styled.h3`
+  font-size: ${(props) => props.subtitleSize}px;
+  font-weight: ${(props) => (props.isBold ? 'bold' : 'normal')};
+  text-shadow: ${(props) =>
+    props.isShadow ? '2px 2px 3px rgba(0,0,0,0.5)' : 'unset'};
+  color: ${(props) => (props.isBlack ? 'black' : 'white')};
+`;
