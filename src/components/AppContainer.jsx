b2c815296b1d6e3c5875dce15a00fbc7843da2e0
kyu3638 <kyu3638@gmail.com>
2023-12-08 16:39:18 +0900

    refactor:불필요 코드 제거

--- src/components/AppContainer.jsx
+++ src/components/AppContainer.jsx
@@ -3,7 +3,6 @@ import '../App.scss';
 import { connect } from 'react-redux';
 import ThumbCreator from './ThumbCreator';
 import ThumbStorage from './ThumbStorage';
-import Nav from './Nav';
 
 function AppContainer(props) {
   return (
